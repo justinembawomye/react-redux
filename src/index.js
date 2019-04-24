a19d35935e5e3bf7c3aa49e893dcc17444f6ef2c
justinembawomye <jmbawomye@gmail.com>
2019-04-24 13:18:29 +0300

    post and get data using react only

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import Posts  from "./Components/Posts";
+import PostForm from "./Components/PostForm";
 
 class App extends React.Component {
   render() {
     return (
       <Fragment>
         <div>Hello {this.props.name}</div>
+        <PostForm/>
+        <hr/>
         <Posts />
       </Fragment>
     );
